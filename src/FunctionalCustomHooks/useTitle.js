c785533acfe32c3b1cbbd93db0e61624ff85c987
Izaz Ahamed Mallick <izazahamedmallick98@gmail.com>
2024-10-14 10:21:04 +0530

    #

--- src/FunctionalCustomHooks/useTitle.js
+++ src/FunctionalCustomHooks/useTitle.js
@@ -2,6 +2,6 @@ import React, { useEffect } from "react";
 
 export const useTitle = (title) => {
     useEffect(() => {
-        document.title = `Doctor Concult || ${title}`;
+        document.title = `Doctor Consult || ${title}`;
     }, [title]);
 };
